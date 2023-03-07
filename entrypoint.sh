bf2fa1dd61b2a116d6a3ca1b1b6dbefefd9202d1
MisakaNo の 小破站 <122191366+Misaka-blog@users.noreply.github.com>
2023-03-07 08:49:06 +0800

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+# 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
+UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/vm'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/vl'}
+TROJAN_WSPATH=${TROJAN_WSPATH:-'/tr'}
+sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g" config.json
+sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g" /etc/nginx/nginx.conf
+sed -i "s#RELEASE_RANDOMNESS#${RELEASE_RANDOMNESS}#g" /etc/supervisor/conf.d/supervisord.conf
+
+# 设置 nginx 伪装站
+rm -rf /usr/share/nginx/*
+wget https://gitlab.com/Misaka-blog/xray-paas/-/raw/main/mikutap.zip -O /usr/share/nginx/mikutap.zip
+unzip -o "/usr/share/nginx/mikutap.zip" -d /usr/share/nginx/html
+rm -f /usr/share/nginx/mikutap.zip
+
+# 伪装 Sing-box 执行文件
+RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
+mv sing-box ${RELEASE_RANDOMNESS}
+cat config.json | base64 > config
+rm -f config.json
+
+# 如果有设置哪吒探针三个变量,会安装。如果不填或者不全,则不会安装
+[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY}
+
+# 启用 Argo，并输出节点日志
+cloudflared tunnel --url http://localhost:80 --no-autoupdate > argo.log 2>&1 &
+sleep 5 && argo_url=$(cat argo.log | grep -oE "https://.*[a-z]+cloudflare.com" | sed "s#https://##")
+
+# 输出节点链接
+vmlink=$(echo -e '\x76\x6d\x65\x73\x73')://$(echo -n "{\"v\":\"2\",\"ps\":\"Argo_sing-box_vmess\",\"add\":\"$argo_url\",\"port\":\"443\",\"id\":\"$UUID\",\"aid\":\"0\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"$argo_url\",\"path\":\"$VMESS_WSPATH?ed=2048\",\"tls\":\"tls\"}" | base64 -w 0)
+vllink=$(echo -e '\x76\x6c\x65\x73\x73')"://"$UUID"@"$argo_url":443?encryption=none&security=tls&type=ws&host="$argo_url"&path="$VLESS_WSPATH"?ed=2048#Argo_sing-box_vless"
+trlink=$(echo -e '\x74\x72\x6f\x6a\x61\x6e')"://"$UUID"@"$argo_url":443?security=tls&type=ws&host="$argo_url"&path="$TROJAN_WSPATH"?ed2049#Argo_sing-box_trojan"
+
+qrencode -o /usr/share/nginx/html/M$UUID.png $vmlink
+qrencode -o /usr/share/nginx/html/L$UUID.png $vllink
+qrencode -o /usr/share/nginx/html/T$UUID.png $trlink
+
+cat > /usr/share/nginx/html/$UUID.html<<-EOF
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Argo-sing-box-paas</title>
+    <style type="text/css">
+        body {
+            font-family: Geneva, Arial, Helvetica, san-serif;
+        }
+
+        div {
+            margin: 0 auto;
+            text-align: left;
+            white-space: pre-wrap;
+            word-break: break-all;
+            max-width: 80%;
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+<body bgcolor="#FFFFFF" text="#000000">
+    <div>
+        <font color="#009900"><b>VMESS协议链接：</b></font>
+    </div>
+    <div>$vmlink</div>
+    <div>
+        <font color="#009900"><b>VMESS协议二维码：</b></font>
+    </div>
+    <div><img src="/M$UUID.png"></div>
+    <div>
+        <font color="#009900"><b>VLESS协议链接：</b></font>
+    </div>
+    <div>$vllink</div>
+    <div>
+        <font color="#009900"><b>VLESS协议二维码：</b></font>
+    </div>
+    <div><img src="/L$UUID.png"></div>
+    <div>
+        <font color="#009900"><b>TROJAN协议链接：</b></font>
+    </div>
+    <div>$trlink</div>
+    <div>
+        <font color="#009900"><b>TROJAN协议二维码：</b></font>
+    </div>
+    <div><img src="/T$UUID.png"></div>
+</body>
+</html>
+EOF
+
+nginx
+base64 -d config > config.json
+./${RELEASE_RANDOMNESS} run -c config.json
